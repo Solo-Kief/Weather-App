e321b3b15a0e0d4055bb563b376fb56d48a6e7b6
Solomon Kieffer <solomonkeiffer@MacBook-Pro-80.local>
2018-10-25 12:38:09 -0500

    API Key file made.

---
+++ Weather App/API.swift
@@ -0,0 +1,12 @@
+//  API.swift
+//  Weather App
+//
+//  Created by Solomon Kieffer on 10/25/18.
+//  Copyright © 2018 Phoenix Development. All rights reserved.
+
+import Foundation
+
+struct APIKeys {
+    let darkSkyKey = ""
+    let googleKey = ""
+}
